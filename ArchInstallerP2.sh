f8c9b8ca922546d94e623048dc35296e464af0ce
CatPerson-co-uk <98697886+CatPerson-co-uk@users.noreply.github.com>
2022-01-30 19:32:05 +0000

    fixed line 29

--- ArchInstallerP2.sh
+++ ArchInstallerP2.sh
@@ -9,7 +9,7 @@ Please Exit The Game After The Installer Has Finished
 \e[0m"
 sleep 5
 osu-wine
-# Installing Old Version Of Wine
+# Installing Old Version Owell if Wine
 echo -e "\e[36m
 --------------------------------
 Now Installing Old Wine Version
@@ -26,7 +26,7 @@ sleep 1
 xdg-open https://drive.google.com/file/d/1xgJIe18ccBx6yjPcmBxDbTnS1XxwrAcc/view
 read -sn 1
 tar -xf ~/Downloads/wine-osu-7.0-x86_64.pkg.tar.zst
-sudo cp -r ~/Downloads/opt/wine-osu/ /opt
+sudo cp -r opt/wine-osu /opt
 sudo sed -i -e 's/#PATH/PATH/g' /etc/osu-wine.conf
 sudo cp /etc/osu-wine.conf ~/.osu-wine.conf
 sudo wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Zu5vK-ACs1KhF4Rju1HAIIPb6XC42s9X' -O /usr/bin/osu-wine
