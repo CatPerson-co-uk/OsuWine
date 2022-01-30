ff1cc88b79487e5ad3c9269935ab9d79f5b8dba3
CatPerson-co-uk <98697886+CatPerson-co-uk@users.noreply.github.com>
2022-01-30 15:37:06 +0000

    Add files via upload

---
+++ ArchInstallerP1.sh
@@ -0,0 +1,70 @@
+
+
+# Dependancies
+sudo pacman -Sy
+sudo pacman -S --noconfirm \
+    git \
+    p7zip \
+    wget \
+    wine-staging \
+    winetricks \
+    giflib \
+    lib32-giflib \
+    libpng \
+    lib32-libpng \
+    libldap \
+    lib32-libldap \
+    gnutls \
+    lib32-gnutls \
+    mpg123 \
+    lib32-mpg123 \
+    openal \
+    lib32-openal \
+    v4l-utils \
+    lib32-v4l-utils \
+    libpulse \
+    lib32-libpulse \
+    alsa-plugins \
+    lib32-alsa-plugins \
+    alsa-lib \
+    lib32-alsa-lib \
+    libjpeg-turbo \
+    lib32-libjpeg-turbo \
+    libxcomposite \
+    lib32-libxcomposite \
+    libxinerama \
+    lib32-libxinerama \
+    ncurses \
+    lib32-ncurses \
+    opencl-icd-loader \
+    lib32-opencl-icd-loader \
+    libxslt \
+    lib32-libxslt \
+    libva \
+    lib32-libva \
+    gtk3 \
+    lib32-gtk3 \
+    gst-plugins-base-libs \
+    lib32-gst-plugins-base-libs \
+    vulkan-icd-loader \
+    lib32-vulkan-icd-loader \
+    cups \
+    samba \
+    dosbox
+
+# Install Pipewire / Remove pulseaudio
+sudo pacman -Rdd pulseaudio
+sudo pacman -S \
+  pipewire \
+  pipewire-pulse \
+  pipewire-jack \
+  pipewire-alsa \
+  wireplumber
+
+echo -e "\e[36m
+----------------------------------------------------
+Computer Will Now Restart. Run Part 2 After Reboot.
+----------------------------------------------------
+\e[0m"
+sleep 3
+reboot
---
+++ ArchInstallerP2.sh
@@ -0,0 +1,37 @@
+# Install Osu
+git clone https://gitlab.com/osu-wine/osu-wine
+cd osu-wine
+sudo ./install.sh
+echo -e "\e[36m
+-------------------------------------------------------
+Please Exit The Game After The Installer Has Finished
+------------------------------------------------------
+\e[0m"
+sleep 5
+osu-wine
+# Installing Old Version Of Wine
+echo -e "\e[36m
+--------------------------------
+Now Installing Old Wine Version
+--------------------------------
+\e[36m
+"
+sleep 1
+echo "
+Please download this file,
+Once the file has been downloaded press "Return".
+(do not move the file from Downloads)
+"
+sleep 1
+xdg-open https://drive.google.com/file/d/1xgJIe18ccBx6yjPcmBxDbTnS1XxwrAcc/view
+read -sn 1
+tar -xf ~/Downloads/wine-osu-7.0-x86_64.pkg.tar.zst
+sudo cp -r ~/Downloads/opt/wine-osu/ /opt
+sudo sed -i -e 's/#PATH/PATH/g' /etc/osu-wine.conf
+sudo cp /etc/osu-wine.conf ~/.osu-wine.conf
+sudo wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Zu5vK-ACs1KhF4Rju1HAIIPb6XC42s9X' -O /usr/bin/osu-wine
+echo -e "\e[36m
+--------------------------------
+      Installation Finished
+--------------------------------
+"
